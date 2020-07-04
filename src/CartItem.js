ec239f1a268bca6b8d3873c1a79d3021a61c2805
Sourabh Majumdar <majumdars844@gmail.com>
2020-07-04 18:49:04 +0530

    Add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQuantity = () =>{
+        console.log(this);
+    }
     render(){
+        const {price, title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,14 +23,29 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>{price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Button to add substract and delete */}
+                        <img 
+                            alt="Increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/992/992651.svg"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img
+                            alt="Decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/992/992683.svg"
+                        />
+                        <img 
+                            alt="Delete" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/1345/1345823.svg"
+                        />
                     </div>
                 </div>
-
             </div>
         );
     }
